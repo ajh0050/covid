a0b034524e018e701ea372702969e51d651370b4
ajh0050 <adamjordanhughes@gmail.com>
2020-04-06 17:34:13 -0700

    ternary chart

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RenderLineChart from  './chart';
 import DeathData from './DeathData';
 import RenderRadialChart from './AgeRadialChart.js';
 import AgeData from './AgeData';
-import { Grid } from '@material-ui/core'
+import { Grid, Card } from '@material-ui/core'
 
 function App() {
-  console.log(AgeData())
+  const [clicked, setClicked] = useState(false)
 
   return (
     <section>
       <Grid>
-    <div style={{height: 500}}>
-      <RenderLineChart data={DeathData()}/>
-    </div>
+    <Card style={{height: 500}}>
+      {clicked ? <RenderLineChart data={DeathData()}/> : <button onClick={()=>setClicked(true)}>Click me</button>}
+    </Card>
     
     <div style={{height: 700, width: 700}}>
       <RenderRadialChart data={AgeData()}/>
